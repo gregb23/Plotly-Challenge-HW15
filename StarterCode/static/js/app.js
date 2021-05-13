a4919ff51cfbd255f92fb6ab6f855a41c7737ad4
Gregory Brown <gregory.brown23@gmail.com>
2021-05-13 18:48:02 -0400

    set up change event

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,5 @@
 // set open browser function
-//function init(){
+function init(){
     //read data with d3
     d3.json("../data/samples.json").then((data) => {
         console.log(data);
@@ -25,13 +25,24 @@
         let sample_Values = sampleSort.map((samp) => samp.sample_Values);
         let label = sampleSort.map((label) => label.otu_labels);
 
-        //parse out ids from metadata id
+        //parse out ids from metadata idti
         let metaIDs = parseInt(id);
 
         //get demo data for each id
         let demoidFilter = data.metadata.filter((demo) => demo.id === metaIDs);
         let demo = demoidFilter[0];
+
+        //build plots
+        buildPlots(otu_ID, sample_Values, label);
+
+        //get demo info from demoidfilter function
+        metaData(demo);
+
+        //set up change event for dropdown
+        mySelect.on("change", () => handleChange(mySelect));
+
     });
+}    
     
 
     
