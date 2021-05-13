eaee9f9cdcb6fafa307954ca610e50fbd85371f5
Gregory Brown <gregory.brown23@gmail.com>
2021-05-13 18:05:27 -0400

    able to see data in console

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,10 +1,14 @@
 // set open browser function
-function init(){
+//function init(){
     //read data with d3
     d3.json("../data/samples.json").then((data) => {
         console.log(data);
+
         let mySelect = d3.select("#selDataset");
-        data.names.forEach((element)=> {
-            mySelect.append("options").attr("value", element).text(element);
+        data.names.forEach((element) => {
+            mySelect.append("option").attr("value", element).text(element);
         });
-}
+    });
+    
+
+    
