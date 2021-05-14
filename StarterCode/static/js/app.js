efcd31d9786a0b0322d1adac561ef22646d01bcb
Gregory Brown <gregory.brown23@gmail.com>
2021-05-13 21:17:12 -0400

    got plots to work

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -22,7 +22,7 @@ function init(){
 
         //create map to get data for bar and bubble chart
         let otu_ID = sampleSort.map((otu) => otu.otu_ids);
-        let sample_Values = sampleSort.map((samp) => samp.sample_Values);
+        let sampleValues = sampleSort.map((samp) => samp.sample_Values);
         let label = sampleSort.map((label) => label.otu_labels);
 
         //parse out ids from metadata idti
@@ -30,32 +30,79 @@ function init(){
 
         //get demo data for each id
         let demoidFilter = data.metadata.filter((demo) => demo.id === metaIDs);
-        let demo = demoidFilter[0];
+        let demos = demoidFilter[0];
 
         //set up change event for dropdown
         mySelect.on("change", () => handleChange(mySelect));
 
         //build plots
-        buildPlots(otu_ID, sample_Values, label);
+        buildPlots(otu_ID, sampleValues, label);
 
         //get demo info from demoidfilter function
-        metaData(demo);
+        metaData(demos);
         
     });
 }
 
 //create plots 
-function buildPlots(otu_ID, sample_Values, label) {
-    otu = otu_ID[0].slice(0, 15).reverse();
-    vals = sample_Values[0].slice(0,15).reverse();
-    hoverText = label[0].slice(0, 15).reverse();
+function buildPlots(otu_ID, sampleValues, label) {
+    otu = otu_ID[0].slice(0, 10).reverse();
+    vals = sampleValues[0].slice(0, 15).reverse();
+    hoverText = label[0].slice(0, 10).reverse();
+
 
     //create labesls 
     otus = [];
     otu.forEach((title) => otus.push(`OTU ${title}`));
+
+    //build bar chart
+    let trace1 = {
+        type: "bar",
+        orientation: "h",
+        x: vals,
+        y: otus,
+        text: hoverText,
+        marker: {color: vals, colorscale: "Portland"},
+    };
+
+    //plot 
+    let dataBar = [trace1];
+    Plotly.newPlot("bar", dataBar);
+
+    //build bubble chart
+    let bubbleY = sampleValues[0];
+    let bubbleX = otu_ID[0];
+    let bubbleHover = label[0];
+
+    //set up trace
+    let trace2 = {
+        text: bubbleHover,
+        x: bubbleX,
+        y: bubbleY,
+        mode: "markers",
+        marker: {color: bubbleX, size: bubbleY, colorscale: "Portland"},
+    };
+
+    //plot
+    let dataBubble = [trace2];
+    Plotly.newPlot("bubble", dataBubble);
 }
 
+//set up information to fill demo 
+function metaData(demos) {
+    let demoInfo= d3.select("#sample-metadate");
+    demoInfo.html("");
+    Object.entries(demos).forEach(([key, value]) => {
+        demoInfo.append("option").text(`${key}: ${value}`);
+    });
 
-    
+//set up change function
+handleChange = () => {
+    d3.event.preventDefault();
+    let id = d3.select("#setDataset").property("value");
+    init(id);
+};
 
-    
+}
+
+init();
